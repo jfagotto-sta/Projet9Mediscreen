c8a1711785e6947c666d90afaefb68baa787bc46
Fagotto <joffrey.fagotto4@laposte.net>
2022-11-25 14:03:04 +0100

    project initialzation

---
+++ src/main/java/com/projet9/mediscreen/projet9/Projet9Application.java
@@ -0,0 +1,13 @@
+package com.projet9.mediscreen.projet9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet9/mediscreen/projet9/Projet9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet9.mediscreen.projet9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
